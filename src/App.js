ca7b9d4f48b675a0749aff5e4e773a216915df3d
austynwhaley <austyn_whaley@yahoo.com>
2022-05-27 15:39:48 -0400

    added 'pages' dir and additional tweaks to homepage

--- src/App.js
+++ src/App.js
@@ -12,23 +12,37 @@ export default function App() {
     <div className='App'>
       <Navbar/>;
       <Header/>;
+      {/*Summary Card*/}
       <Card
       bootstrap="mr-auto card w-50"
       title= "Summary"
       animate="fade-right">
       <p style={{color:'white'}}>I am a dedicated and adaptable employee who  handles fast-paced work. Proven willingness to take on any task to support team and help business succeed. Offers strong time management and problem solving abilities. </p>
+      <div className='links'>
+        <a href=''>Email</a>|
+        <a href='https://www.linkedin.com/in/austyn-whaley-a820421b5/'>LinkedIn</a>| 
+        <a href='https://github.com/austynwhaley'>Github</a>| 
+        <a href='https://www.instagram.com/whaleanator/'>Instagram</a>| 
+        <a href='https://twitter.com/whaleanator'>Twitter</a></div>
+        
+      {/* insert email, linked in, github, insta, twitter links */}
 
       </Card>;
+      {/*Toolkit Card*/}
       <Card
       bootstrap="ml-auto card w-50"
       title= "Toolkit"
-      animate="fade-left"
-      />;
+      animate="fade-left">
+      <p className='toolkit'>HTML, CSS, Javascript, jQuery, Bootstrap, Bulma, Node.js, MySQL, MongoDB, Git, Postman, React.js, PHP, C#, C++</p>
+      </Card>;
+      {/*HTML, CSS, Javascript, jQuery, Bootstrap, Bulma, Node.js, MySQL, MongoDB, Git, Postman, React.js, PHP, C#, C++,*/}
+      {/*Projects Card*/}
       <Card
-      bootstrap="mr-auto card w-50"
+      bootstrap="mx-auto card w-50"
       title= "Projects"
-      animate="fade-right"
-      />;
+      animate="fade-right">
+        
+      </Card>
     </div>
       
   );
