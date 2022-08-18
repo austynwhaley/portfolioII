1b1c1a1655ae630751c8d2a346e39ae7bd91579f
austynwhaley <austyn_whaley@yahoo.com>
2022-08-18 17:18:28 -0400

    added resume page

---
+++ src/pages/Resume/index.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+
+const Resume = () => {
+return(
+    <div>
+        <h1>Resume</h1>
+    </div>
+)
+
+};
+
+export default Resume;
