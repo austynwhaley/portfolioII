f0659851720e5595372a6ce29ca9eba260a8a3cc
austynwhaley <austyn_whaley@yahoo.com>
2022-08-28 23:22:44 -0400

    about me update

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -13,6 +13,7 @@ function About() {
         bootstrap="mx-auto card w-50"
         title= "About Me"
         animate="fade-right">
+          <hr/>
              <div className="info">
                 <p style={{fontSize: '1.4rem'}}>My Name is <span>Austyn Whaley</span>, I am 28 Years old and I was born in <span>Cincinnati Ohio</span>.</p> 
                 <p style={{fontSize: '1.4rem'}}>I grew up all around different areas in Northern Kentucky and went to a few different schools before I ended up in Belleuve,KY; and there is where I graduated from Bellevue High School in 2012.</p>
@@ -20,6 +21,15 @@ function About() {
                 <p style={{fontSize: '1.4rem'}}> I have a wide range of development interests from <span>blockchain</span> and <span>Web3</span> development with smart contracts, to <span>game</span> development.</p>
                 <p style={{fontSize: '1.4rem'}}> I'm <span>hard working </span>and a <span>dedicated</span> developer constantly looking for new projects, new ideas and opportunities</p>
                 <p style={{fontSize: '1.4rem'}}> Thanks for reading!</p>
+              </div>
+              <hr/>
+              <div className='linksCon'>
+                <p className='spacer'>
+                  <a className='links' href='https://www.linkedin.com/in/austyn-whaley-a820421b5/'>LinkedIn</a>|
+                  <a className='links' href='https://github.com/austynwhaley'>GitHub</a>|
+                  <a href='https://www.instagram.com/whaleanator/'>Instagram</a>|
+                  <a href='https://twitter.com/whaleanator'>Twitter</a>
+                </p>
               </div>
         </Card>
         
