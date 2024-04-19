9e8bf77e21da91a64e2f7618766d42e27d818f3e
austynwhaley <austyn_whaley@yahoo.com>
2024-04-18 22:18:04 -0400

    typo

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -38,7 +38,7 @@ const About = () => {
                   I was hesitant to commit to a path without a clear idea of what skills I wanted to develop for a career, and I didn't want to accumulate unnecessary debt.
                    Instead, I landed a job at a family company run by one of my close friends. For five years, I worked as a woodworker, restoring antique furniture. 
                    While there were aspects of the job I enjoyed, I never saw it as my long-term career. In truth, I'd always been drawn to Computer Science, but right after high school,
-                    I lacked confidence in pursuing it and had no guidance. However, in 2020, I finally took the plunge and enrolled in a <span>Full Stack Engineering</span> 
+                    I lacked confidence in pursuing it and had no guidance. However, in 2020, I finally took the plunge and enrolled in a <span>Full Stack Engineering </span> 
                     program offered by <span>The Ohio State University</span>. That decision marked the beginning of my career journey.</p>
 
                 <p>I currently I work at <span>Fischer Homes</span> on the development team as an <span>Software Engineer</span>.</p>
@@ -47,7 +47,7 @@ const About = () => {
                  to <span>AI</span> and <span>Web3</span> development and <span>Machine Learning</span>, and even to game development with <span>Unreal</span> and <span>Unity</span>.</p>
 
                 <p> I'm <span>hard working </span>and a <span>dedicated</span> developer constantly looking for new ideas and opportunities to strenghten my skills and knowledge</p>
-                
+
                 <p> Thanks for reading!</p>
               </div>
               <hr/>
